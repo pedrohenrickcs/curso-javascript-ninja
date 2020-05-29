0c604fcd1b82f42c4a6b6741a9cf6db3d8d2e573
unknown <pedro.henrickcs@gmail.com>
2020-05-29 12:58:30 -0300

    :sparkles: challenge-28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,4 +1,4 @@
-(function () {
+(function (DOM) {
 	'use-strict';
 	/*
 	No HTML:
@@ -27,84 +27,6 @@
 	- Utilize a lib DOM criada anteriormente para facilitar a manipulação e
 	adicionar as informações em tela.
 	*/
-
-	function DOM(elements) {
-		this.element = document.querySelectorAll(elements);
-	}
-
-	DOM.prototype.on = function on(eventType, callback) {
-		Array.prototype.forEach.call(this.element, function (element) {
-			element.addEventListener(eventType, callback, false);
-		});
-	}
-
-	DOM.prototype.off = function off(eventType, callback) {
-		Array.prototype.forEach.call(this.element, function (element) {
-			element.removeEventListener(eventType, callback, false);
-		});
-	}
-
-	DOM.prototype.get = function get() {
-		return this.element;
-	}
-
-	DOM.prototype.forEach = function forEach() {
-		return Array.prototype.forEach.apply(this.element, arguments)
-	}
-
-	DOM.prototype.map = function Map() {
-		return Array.prototype.map.apply(this.element, arguments)
-	}
-
-	DOM.prototype.filter = function filter() {
-		return Array.prototype.filter.apply(this.element, arguments)
-	}
-
-	DOM.prototype.reduce = function reduce() {
-		return Array.prototype.reduce.apply(this.element, arguments)
-	}
-
-	DOM.prototype.reduceRight = function reduceRight() {
-		return Array.prototype.reduceRight.apply(this.element, arguments)
-	}
-
-	DOM.prototype.every = function every() {
-		return Array.prototype.every.apply(this.element, arguments)
-	}
-
-	DOM.prototype.some = function some() {
-		return Array.prototype.some.apply(this.element, arguments)
-	}
-
-	DOM.isArray = function isArray(param) {
-		return Object.prototype.toString.call(param) === '[object Array]';
-	}
-
-	DOM.isObject = function isObject(param) {
-		return Object.prototype.toString.call(param) === '[object Object]';
-	}
-
-	DOM.isFunction = function isFunction(param) {
-		return Object.prototype.toString.call(param) === '[object Function]';
-	}
-
-	DOM.isNumber = function isNumber(param) {
-		return Object.prototype.toString.call(param) === '[object Number]';
-	}
-
-	DOM.isString = function isString(param) {
-		return Object.prototype.toString.call(param) === '[object String]';
-	}
-
-	DOM.isBoolean = function isBoolean(param) {
-		return Object.prototype.toString.call(param) === '[object Boolean]';
-	}
-
-	DOM.isNull = function isNull(param) {
-
-		return Object.prototype.toString.call(param) === '[object Null]' || Object
-			.prototype.toString.call(param) === '[object Undefined]';
-	}
 
 	var ajax = new XMLHttpRequest();
 	var $formCep = new DOM('[data-js="form-cep"]');
@@ -204,4 +126,4 @@
 
 	initialize();
 
-})();
+})(window.DOM);
